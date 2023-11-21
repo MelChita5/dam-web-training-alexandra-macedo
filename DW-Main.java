6023c5f246a81a8c68b250cd10015e0155360d80
AlexandraMLCHT <140069448+MelChita5@users.noreply.github.com>
2023-11-21 14:05:08 +0100

    Update DW-Main.java

---
+++ DW-Main.java
@@ -0,0 +1,12 @@
+public class ContadorHasta5 {
+    public static void main(String[] args) {
+        // Inicializar el contador
+        int contador = 1;
+
+        // Utilizar un bucle while para contar hasta 5
+        while (contador <= 5) {
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+    }
+}
